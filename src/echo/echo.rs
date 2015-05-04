b573de61da8fc128224daad56d24fd4aef9e9b86
Marco don Kaulea <DonMarco42@gmail.com>
2015-05-04 21:52:26 +0200

    Use getopt to parse -n
    This version still prints options

--- src/echo/echo.rs
+++ src/echo/echo.rs
@@ -1,11 +1,21 @@
 #![feature(collections)]
+extern crate getopts;
+use getopts::Options;
 use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
+    opts.optflag("n", "", "do not output the trailing newline");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
 
     for argument in args.tail() {
         print!("{} ", argument);
     }
-    print!("\n");
+    if !matches.opt_present("n") {
+        print!("\n");
+    }
 }
