1581430b487f0c4b35a9603a3e0f169fb2afb650
Marco don Kaulea <DonMarco42@gmail.com>
2015-05-06 18:51:50 +0200

    Add parameter parsing with docopt
    THIS WILL OVERRIDE YOUR FILES!!

--- src/touch/touch.rs
+++ src/touch/touch.rs
@@ -1,15 +1,37 @@
-use std::env;
+extern crate rustc_serialize;
+extern crate docopt;
+
+use docopt::Docopt;
 use std::fs::File;
 use std::io::Write;
 
+static USAGE: &'static str = "
+Usage: touch <file>...
+";
+
+#[derive(RustcDecodable, Debug)]
+struct Args {
+    arg_file: Vec<String>,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut f = match File::create(&args[1]) {
-    Err(e) => {
-        println!("Couldn't open foo.txt {}", e);
-        return;
-    },
-    Ok(f) => f,
-};
-    println!("{:?}", f.write_all(b""));
+    let args: Args = Docopt::new(USAGE)
+                             .and_then(|d| d.decode())
+                             .unwrap_or_else(|e| e.exit());
+    for name in args.arg_file {
+        let mut f = match File::create(&name) {
+            Err(e) => {
+                println!("Couldn't open {}: {}", &name, e);
+                return;
+            },
+            Ok(f) => f,
+        };
+        match f.write_all(b"") {
+            Err(e) => {
+                println!("Couldn't write {}: {}", &name, e);
+                continue;
+            },
+            Ok(_) => (),
+        };
+    }
 }
