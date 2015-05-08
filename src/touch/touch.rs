2274f447acac2b9aeb14d34b68a919d771bcb94e
Marco don Kaulea <DonMarco42@gmail.com>
2015-05-08 21:55:54 +0200

    Implement --no-create flag

--- src/touch/touch.rs
+++ src/touch/touch.rs
@@ -8,31 +8,44 @@ use std::fs::OpenOptions;
 use std::fs::set_file_times;
 
 static USAGE: &'static str = "
-Usage: touch <file>...
+Usage: touch [-c] <file>...
+
+Options:
+    -c, --no-create     do not create any files
 ";
 
 #[derive(RustcDecodable, Debug)]
 struct Args {
     arg_file: Vec<String>,
+    flag_no_create: bool,
 }
 
 fn main() {
     let args: Args = Docopt::new(USAGE)
                              .and_then(|d| d.decode())
                              .unwrap_or_else(|e| e.exit());
+
     for name in args.arg_file {
-        match OpenOptions::new()
+        if !args.flag_no_create {
+            match OpenOptions::new()
                                      .read(true)
                                      .create(true)
                                      .open(&name) {
+                Err(e) => {
+                    println!("Couldn't open {}: {}", &name, e);
+                    return;
+                },
+                Ok(_) => (),
+            };
+        }
+        let now = (time::get_time().sec * 1000) as u64;
+        match std::fs::set_file_times(std::path::Path::new(&name), now, now) {
             Err(e) => {
-                println!("Couldn't open {}: {}", &name, e);
-                return;
+                if !args.flag_no_create {
+                    println!("Couldn't write time for {}: {}", &name, e);
+                }
             },
             Ok(_) => (),
         };
-        let now = (time::get_time().sec * 1000) as u64;
-        std::fs::set_file_times(std::path::Path::new(&name), now, now)
-         .unwrap_or_else(|e| println!("Couldn't write {}: {}", &name, e));
     }
 }
